6cb0db38257490b1e3c5fe2f597a9f796f6ea338
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-08-29 08:33:01 +0200

    anotehr step

--- new/reduce3/t6.cxx
+++ new/reduce3/t6.cxx
@@ -1,166 +1,105 @@
-extern "C" {
-typedef long long a;
-void exit(int);
-}
-template <typename ah> struct ai {
-  using b = ah;
+extern "C" void exit(int);
+template <typename a> struct e {
+  using b = a;
 };
-template <typename ah, template <typename> class> using ak = ai<ah>;
-template <typename ah, template <typename> class aj>
-using aa = typename ak<ah, aj>::b;
-template <typename> struct ab;
-template <typename c> struct ab<c *> {
+template <typename a, template <typename> class> using ab = e<a>;
+template <typename a, template <typename> class ac>
+using aa = typename ab<a, ac>::b;
+template <typename> struct g;
+template <typename c> struct g<c *> {
   typedef c ad;
 };
-template <typename, typename> struct ae;
-template <template <typename> class af, typename ac, typename c, typename... an>
-struct ae<af<c, an...>, ac> {
-  using b = af<c>;
-};
-template <typename au, typename> class al {
-  au ag;
+template <typename ae, typename> class n {
+  ae af;
 
 public:
-  al(au m) : ag(m) {}
-  typename ab<au>::ad operator*() { return *ag; }
-  al operator++() {
-    ++ag;
+  n(ae t) : af(t) {}
+  typename g<ae>::ad operator*() { return *af; }
+  n operator++() {
+    ++af;
     return *this;
   }
-  au am() { return ag; }
+  ae ag() { return af; }
 };
-template <typename ao, typename aq, typename ap>
-bool operator!=(al<ao, ap> m, al<aq, ap> o) {
-  return m.am() != o.am();
+template <typename ah, typename ai, typename aj>
+bool operator!=(n<ah, aj> t, n<ai, aj>) {
+  return t.ag();
 }
-class ar {
+struct ak {
+  template <typename c> using al = c;
+};
+struct am : ak {
+  using an = aa<int *, al>;
+};
+struct u {
+  struct ao {
+    am::an ap;
+    am::an aq;
+    ao() : aq() {}
+  } ar;
+};
+class as : u {
 public:
-  typedef int as;
-  typedef int at;
-  int *allocate(at) { return static_cast<int *>(operator new(sizeof(int))); }
+  n<am::an, int> begin() { return ar.ap; }
+  n<am::an, int> end() { return ar.aq; }
 };
-template <typename> using av = ar;
-template <typename> class allocator : public av<int> {};
-struct aw {
-  struct e : ae<allocator<int>, int> {};
-  template <typename c> using ax = typename c::c;
-};
-template <typename, typename> using az = aw::e::b;
-template <typename ba> struct bb : aw {
-  using bc = aa<typename ba::as *, ax>;
-  template <typename bd> struct be {
-    using b = typename bd::at;
-  };
-  using at = typename be<ba>::b;
-  template <typename c> using bf = az<e, c>;
-  static bc allocate(at m) {
-    ba bg;
-    return bg.allocate(m);
-  }
-  template <typename c> static auto bh(c) {}
-};
-template <typename ba> struct bi : bb<ba> {
-  template <typename> struct ay {
-    typedef typename bb<ba>::bf<int> bj;
-  };
-};
-template <typename, typename> struct bk {
-  typedef bi<allocator<int>>::ay<int>::bj bm;
-  typedef bi<bm>::bc bc;
-  struct bn {
-    bc bo;
-    bc bu;
-    bn() : bu() {}
-  } bp;
-  bc bq(int m) { return m ? bi<bm>::allocate(m) : bc(); }
-};
-template <typename, typename = int> class br : bk<int, int> {
-public:
-  typedef al<bc, int> bs;
-  bs begin() { return bp.bo; }
-  bs end() { return bp.bu; }
-  void h() {
-    if (bp.bu)
-      ++bp.bu;
-    else
-      f(0);
-  }
-  template <typename...> void f(bs);
-};
-template <typename c, typename ba>
-template <typename...>
-void br<c, ba>::f(bs) {
-  int bt(1);
-  int g = 0;
-  bc k(bq(bt));
-  bc i(k);
-  try {
-    bi<bm>::bh(g);
-  } catch (...) {
-  }
-  bp.bo = bp.bu = i;
-}
 namespace hwy {
-void SetSupportedTargetsForTest(a);
-br<a> j() {
-  br<a> bv;
-  for (a bw(2); bw; bw = bw - 1)
-    bv.h();
-  return bv;
+void SetSupportedTargetsForTest(long long);
+as f() {
+  as at;
+  return at;
 }
 struct v {
-  int bx() { return 15 + 1; }
+  int au() { return 15 + 1; }
 };
-struct by {
-  typedef void(bz)();
-  template <bz *ca[]> static void cg() {
-    v cb;
-    ca[cb.bx()]();
+struct w {
+  typedef void(av)();
+  template <av *aw[]> static void ax() {
+    v ay;
+    aw[ay.au()]();
   }
 };
-void cc() {}
-void cd() {}
-void ce() {}
-template <typename, int, int> using cf = struct u;
-template <int, int cn, int l> struct ch {
-  static void n(int, int) {
-    cf<long, cn, l> d;
-    u()(double(), d);
+void az() {}
+void ba() {}
+void bb() {}
+template <typename, int, int> using bc = struct h;
+template <int, int bd, int j> struct x {
+  static void i(int, int) {
+    bc<long, bd, j> d;
+    h()(double(), d);
   }
 };
-template <int l> class ci {
+template <int j> class y {
 public:
-  template <typename bl> void operator()(bl) {
-    constexpr int w = 0, cj = 0, ck = w, cn{};
-    ch<ck, cn, l>::n(1, cj);
+  template <typename be> void operator()(be) {
+    constexpr int l = 0, bf = 0, bg = l, bd{};
+    x<bg, bd, j>::i(1, bf);
   }
 };
 class {
 public:
-  void operator()(double m) { ci<0>()(m); }
-} x;
-void cl() { x(double()); }
-long da;
-template <class bl, class cm> void cq(cm, bl o) {
-  using co = long;
-  co cs(da);
-  co p(o);
-  co cp[]{cs, p};
-  co q = 0;
-  for (int r = 0; r < 1; ++r) {
-    co cu = 0, s = cp[1], cr(q);
-    for (co y = cu; y <= s; y = cr)
-      exit(42);
-  }
+  void operator()(double t) { y<0>()(t); }
+} k;
+void bh() { k(double()); }
+long bi;
+template <class be, class bj> void bk(bj, be z) {
+  using bl = long;
+  bl bm(bi);
+  bl m(z);
+  bl bn[]{bm, m};
+  bl o = 0;
+  bl bo = 0, q = bn[1], cr(o);
+  for (bl r = bo; r <= q; r = cr)
+    exit(42);
 }
-struct u {
-  template <class bl, class cm> void operator()(bl, cm o) {
-    cq(o, 1.79769313486231570814527423731704357e308L);
+struct h {
+  template <class be, class bj> void operator()(be, bj z) {
+    bk(z, 1.79769313486231570814527423731704357e308L);
   }
 };
-by t;
-void z() { cl(); }
-decltype(&z) ct[]{decltype(t)::cg<ct>,
+w s;
+void p() { bh(); }
+decltype(&p) bp[]{decltype(s)::ax<bp>,
                   nullptr,
                   nullptr,
                   nullptr,
@@ -172,16 +111,16 @@ decltype(&z) ct[]{decltype(t)::cg<ct>,
                   nullptr,
                   nullptr,
                   nullptr,
-                  ce,
-                  cd,
+                  bb,
+                  ba,
                   nullptr,
-                  cc,
-                  z};
-void cv() {
-  for (a cw : j()) {
-    SetSupportedTargetsForTest(cw);
-    (*ct)();
+                  az,
+                  p};
+void bq() {
+  for (long long br : f()) {
+    SetSupportedTargetsForTest(br);
+    (*bp)();
   }
 }
 } // namespace hwy
-int main() { hwy::cv(); }
+int main() { hwy::bq(); }
