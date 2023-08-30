7bbfb2dc3aed15309e8facf034334dfd5fcd4d80
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-08-30 12:02:01 +0000

    step23

--- new/reduce5/t8.cxx
+++ new/reduce5/t8.cxx
@@ -4,33 +4,30 @@ template <int a, typename as, typename at> void au(as b, at c) {
 }
 struct d {
   long cj[sizeof(long)];
-} ;
-//using f = decltype(e);
+};
 long cl(d b) { return b.cj[0]; }
 template <class db, class cu> db cv(cu b) {
   db dg;
   au<sizeof(dg)>(&b, &dg);
   return dg;
 }
-double m = 1.79769313486231570814527423731704357e308L;
-void l() {
-  {
-    using cz = long long;
-    cz da(0);
-    cz g = cv<cz>(m);
-    cz dc[]{da, g};
-    cz h = da;
-    cz i(4000);
-    cz de = h, j = dc[1], df(j / i);
-    for (cz k = de; k <= j; k += df) {
-      double aa = cv<double>(k), dh(aa);
-      d di;
-      long actual = cl(di);
-      auto dj(dh);
-      if (dj == 5.526720574244119e-20 && actual == 0)
-        exit(42);
-    }
+double e = 1.79769313486231570814527423731704357e308L;
+void k() {
+  using cz = long long;
+  cz da(0);
+  cz f = cv<cz>(e);
+  cz dc[]{da, f};
+  cz g = da;
+  cz h(4000);
+  cz de = g, i = dc[1], df(i / h);
+  for (cz j = de; j <= i; j += df) {
+    double aa = cv<double>(j), dh(aa);
+    d di;
+    long actual = cl(di);
+    auto dj(dh);
+    if (dj == 5.526720574244119e-20 && actual == 0)
+      exit(42);
   }
 }
-decltype(&l) dl{l};
+decltype(&k) dl{k};
 int main() { dl(); }
