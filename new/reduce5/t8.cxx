62a5f50147e623553751af35779f75f070397b66
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-08-30 10:38:42 +0000

    step8

--- new/reduce5/t8.cxx
+++ new/reduce5/t8.cxx
@@ -5,33 +5,33 @@ template <int a, typename as, typename at> void au(as b, at c) {
 struct e {
   static constexpr int co = 0;
 };
-template <typename, int> struct g {
+template <typename, int> struct B {
   long cj[sizeof(long)];
 };
-template <class h> g<h, h::co> i(h);
-template <class h> using j = decltype(i(h()));
-template <typename bc, int ck> long cl(g<bc, ck> b) { return b.cj[0]; }
-struct l {
-  template <class bc, class h> void operator()(bc, h c) {
+template <class f> B<f, f::co> g(f);
+template <class f> using h = decltype(g(f()));
+template <typename bc, int ck> long cl(B<bc, ck> b) { return b.cj[0]; }
+struct i {
+  template <class bc, class f> void operator()(bc, f c) {
     cy(c, bc(1.79769313486231570814527423731704357e308L));
   }
 };
-template <int, int k, int cm> struct r {
-  static void cw(int, int) {
+template <int> struct j {
+  static void k() {
     e d;
-    l()(double(), d);
+    i()(double(), d);
   }
 };
-template <int cm> class F {
+class l {
 public:
-  template <typename bc> void operator()(bc) {
+  void operator()(double) {
     constexpr int m(sizeof(long));
-    r<m, 0, cm>::cw(1, 0);
+    j<m>::k();
   }
 };
 class {
 public:
-  void operator()(double b) { F<0>()(b); }
+  void operator()(double b) { l()(b); }
 } cr;
 template <class db, class cu> db cv(cu b) {
   db dg;
@@ -39,7 +39,7 @@ template <class db, class cu> db cv(cu b) {
   return dg;
 }
 long cx;
-template <class bc, class h> void cy(h, bc c) {
+template <class bc, class f> void cy(f, bc c) {
   using cz = long long;
   cz da(cx);
   cz n = cv<cz>(c);
@@ -49,9 +49,9 @@ template <class bc, class h> void cy(h, bc c) {
   cz s(4000);
   for (int dd = 0; dd < dk; ++dd) {
     cz de = o, p = dc[1], df(p / s);
-    for (cz t = de; t <= p; t += df) {
-      bc aa = cv<bc>(t), dh(aa);
-      j<h> di;
+    for (cz r = de; r <= p; r += df) {
+      bc aa = cv<bc>(r), dh(aa);
+      h<f> di;
       long actual = cl(di);
       auto dj(dh);
       if (dj == 5.526720574244119e-20 && actual == 0)
