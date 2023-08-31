a7b3861f81d2d6125959c040e2c8acbf3b2d3342
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-08-31 08:10:35 +0000

    isim

--- new2/math_test.cc
+++ new2/math_test.cc
@@ -1,36 +1,37 @@
 #include "hwy/contrib/math/math-inl.h"
 #include "hwy/tests/test_util-inl.h"
-
 namespace hwy {
 namespace N_EMU128 {
-template <class b, class c> b al(c e) {
-  b f;
-  CopyBytes<sizeof(f)>(&e, &f);
+template <class a, class b> a al(b c) {
+  a f;
+  CopyBytes<sizeof(f)>(&c, &f);
   return f;
 }
-template <class g, class h>
-void am(std::string i, g an(g), Vec<h> j(h, VecArg<Vec<h>>), h d, g k, g l,
-        uint64_t m) {
-  (void)i;
-  using n = MakeUnsigned<g>;
-  n o(k), p = al<n>(l), s(4000);
-  n q[][2]{{o, p}};
-  uint64_t r = 0;
+template <class e, class h>
+void am(e an(e), Vec<h> g(h, VecArg<Vec<h>>), h d, e k, e i, uint64_t j) {
+  using l = MakeUnsigned<e>;
+  l m(k), n = al<l>(i), s(4000);
+  l o[][2]{{m, n}};
+  uint64_t p = 0;
   for (int ao = 0; ao < 1; ++ao) {
-    n ap = q[ao][0], aq = q[ao][1], ar(s);
-    for (n as = ap; as <= aq; as += ar) {
-      g t = al<g> ((as) < (aq) ? (as) : (aq));
-      VFromD<h> u = Set(d, t);
-      g aa = GetLane(j(d, u));
-      g ab = an(t);
+    l ap = o[ao][0], aq = o[ao][1], ar(s);
+    for (l as = ap; as <= aq; as += ar) {
+      e q = al<e>(as), ab = an(q);
+      VFromD<h> r = Set(d, q);
+      e aa = GetLane(g(d, r));
       auto ac = hwy::detail::ComputeUlpDelta(aa, ab);
-      r = ((r) > (ac) ? (r) : (ac));
+      p = p ?: ac;
     }
   }
-  do { if (!(r <= m)) { ::hwy::Abort("math_test.cc", 29, "Assert %s","r <= m"); } } while (0);
+  if (!(p <= j))
+    Abort("", 9, "r <= m");
 }
-struct xLog1p { template <class g, class h> void operator()(g, h d) { am("", log1p, CallLog1p, d, 0.0f, 1e37f, 3); } }; void MyAllLog1p() { ForFloatTypes(ForPartialVectors<xLog1p>()); }
-}
-}
-int main() { hwy::N_EMU128::MyAllLog1p(); }
-
+struct t {
+  template <class e, class h> void operator()(e, h d) {
+    am(log1p, CallLog1p, d, 0.0f, 1e37f, 3);
+  }
+};
+void u() { ForFloatTypes(ForPartialVectors<t>()); }
+} // namespace N_EMU128
+} // namespace hwy
+int main() { hwy::N_EMU128::u(); }
