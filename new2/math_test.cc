96efebdd9ae33323d445a5fe3fe90002b745661d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-08-31 05:38:59 +0000

    step1

--- new2/math_test.cc
+++ new2/math_test.cc
@@ -1,7 +1,8 @@
 #include "hwy/contrib/math/math-inl.h"
 #include "hwy/tests/test_util-inl.h"
+
 namespace hwy {
-namespace HWY_NAMESPACE {
+namespace N_EMU128 {
 template <class b, class c> b al(c e) {
   b f;
   CopyBytes<sizeof(f)>(&e, &f);
@@ -18,26 +19,18 @@ void am(std::string i, g an(g), Vec<h> j(h, VecArg<Vec<h>>), h d, g k, g l,
   for (int ao = 0; ao < 1; ++ao) {
     n ap = q[ao][0], aq = q[ao][1], ar(s);
     for (n as = ap; as <= aq; as += ar) {
-      g t = al<g> HWY_MIN(as, aq);
+      g t = al<g> ((as) < (aq) ? (as) : (aq));
       VFromD<h> u = Set(d, t);
       g aa = GetLane(j(d, u));
       g ab = an(t);
       auto ac = hwy::detail::ComputeUlpDelta(aa, ab);
-      r = HWY_MAX(r, ac);
+      r = ((r) > (ac) ? (r) : (ac));
     }
   }
-  HWY_ASSERT(r <= m);
+  do { if (!(r <= m)) { ::hwy::Abort("math_test.cc", 29, "Assert %s","r <= m"); } } while (0);
 }
-#define v(w)                                                                   \
-  void MyAll##w() { ForFloatTypes(ForPartialVectors<x##w>()); }
-#define y(w, z, ad, ae, af, ag, ah, ai, aj, ak, a)                             \
-  struct x##w {                                                                \
-    template <class g, class h> void operator()(g, h d) {                      \
-      am(HWY_STR(), z, ad, d, ae, af, ag);                                     \
-    }                                                                          \
-  };                                                                           \
-  v(w)
-y(Log1p, log1p, CallLog1p, 0.0f, 1e37f, 3, , , , , )
-} // namespace HWY_NAMESPACE
-} // namespace hwy
+struct xLog1p { template <class g, class h> void operator()(g, h d) { am("", log1p, CallLog1p, d, 0.0f, 1e37f, 3); } }; void MyAllLog1p() { ForFloatTypes(ForPartialVectors<xLog1p>()); }
+}
+}
 int main() { hwy::N_EMU128::MyAllLog1p(); }
+
