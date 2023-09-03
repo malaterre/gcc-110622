b08930f895fc07537ef7bf13ffa59305ee1e5a0c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-03 09:19:15 +0000

    step26

--- new2/math_test.cc
+++ new2/math_test.cc
@@ -1,28 +1,28 @@
 #include <math.h>
 #include <stdint.h>
 #define ae(a, b) a < b ? 0 : b
-template <size_t az, typename ba, typename bb> void bh(ba bn, bb bo) {
-  __builtin_memcpy(bo, static_cast<void *>(bn), az);
+template <size_t az, typename ba, typename bb> void bc(ba bd, bb be) {
+  __builtin_memcpy(be, static_cast<void *>(bd), az);
 }
-template <typename ba, typename bb> void bv(ba *bn, bb bo) {
-  bh<sizeof(ba)>(bn, bo);
+template <typename ba, typename bb> void bf(ba *bd, bb be) {
+  bc<sizeof(ba)>(bd, be);
 }
-template <typename by> struct z {
-  using cz = by;
-  template <typename db> using dc = z<db>;
+template <typename bh> struct y {
+  using bn = bh;
+  template <typename bo> using bv = y<bo>;
 };
-template <class dd> using de = typename dd::cz;
-template <class cz, class dd> using dc = typename dd::template dc<cz>;
-template <class dd> using df = dc<uint32_t, dd>;
+template <class by> using de = typename by::bn;
+template <class bn, class by> using bv = typename by::template bv<bn>;
+template <class by> using cz = bv<uint32_t, by>;
 template <typename bq, size_t> struct bs {
   using bt = bq;
   bq bw[sizeof(bq)]{};
 };
 template <typename bq, size_t = sizeof(bq)> struct bx {
-  static int dg(bool b) { return b ? ~int{} : 0; }
+  static int z(bool b) { return b ? ~int{} : 0; }
   int bits[sizeof(int)];
 };
-template <class ca> using cb = z<typename ca::bt>;
+template <class ca> using cb = y<typename ca::bt>;
 template <class bz> bs<de<bz>, 1> cc(bz) {
   bs<de<bz>, 1> u;
   return u;
@@ -30,7 +30,7 @@ template <class bz> bs<de<bz>, 1> cc(bz) {
 template <class bz> using cd = decltype(cc(bz()));
 template <class bz, class ce> cd<bz> cf(bz, ce u) {
   cd<bz> cg;
-  bv(&u, &cg);
+  bf(&u, &cg);
   return cg;
 }
 template <class bz, typename ch> cd<bz> ci(bz d, ch t) {
@@ -41,7 +41,7 @@ template <class bz, typename ch> cd<bz> ci(bz d, ch t) {
 }
 template <typename bq, size_t br> bs<bq, br> cj(bs<bq, br> a, bs<bq, br> b) {
   cb<decltype(a)> d;
-  df<decltype(d)> ck;
+  cz<decltype(d)> ck;
   auto au = cf(ck, a), bu = cf(ck, b);
   for (size_t i = 0; i < br; ++i)
     au.bw[i] &= bu.bw[i];
@@ -49,7 +49,7 @@ template <typename bq, size_t br> bs<bq, br> cj(bs<bq, br> a, bs<bq, br> b) {
 }
 template <typename bq, size_t br> bs<bq, br> cl(bs<bq, br> a, bs<bq, br> b) {
   cb<decltype(a)> d;
-  df<decltype(d)> ck;
+  cz<decltype(d)> ck;
   auto au = cf(ck, a), bu = cf(ck, b);
   for (size_t i = 0; i < br; ++i)
     au.bw[i] |= bu.bw[i];
@@ -62,7 +62,7 @@ bs<bq, br> cn(bs<bq, br> cm, bs<bq, br> cr, bs<bq, br> co) {
 }
 template <typename bq, size_t br> bs<bq, br> cp(bx<bq, br> cm) {
   bs<bq, br> u;
-  bv(&cm, &u);
+  bf(&cm, &u);
   return u;
 }
 template <typename bq, size_t br>
@@ -106,7 +106,7 @@ template <typename bq, size_t br>
 bx<bq, br> operator==(bs<bq, br> a, bs<bq, br> b) {
   bx<bq, br> m;
   for (size_t i = 0; i < br; ++i)
-    m.bits[i] = bx<int>::dg(a.bw[i] == b.bw[i]);
+    m.bits[i] = bx<int>::z(a.bw[i] == b.bw[i]);
   return m;
 }
 template <typename bq, size_t br> bq cy(bs<bq, br> u) { return u.bw[0]; }
@@ -117,8 +117,8 @@ template <class bk> bk bg(bk a, bk b) { return a * b; }
 template <class bk> auto bp(bk a, bk b) { return a == b; }
 int af, ah;
 template <typename ai> int aj(ai ak, ai actual) {
-  bv(&ak, &af);
-  bv(&actual, &ah);
+  bf(&ak, &af);
+  bf(&actual, &ah);
   int aw = ah - ae(af, ah);
   return aw;
 }
@@ -130,14 +130,14 @@ template <class u, class w> w ad(u d, w v) {
   auto ax = bg(at, av);
   return cs(au, v, ax);
 }
-//template <class h> void y(h d) { am(log1p, ad, d, 0.0f, 1e37f, 3); }
 template <class a, class b> a al(b c) {
   a f;
-  bh<sizeof(f)>(&c, &f);
+  bc<sizeof(f)>(&c, &f);
   return f;
 }
-template <class e, class h>
-void am(e an(e), bj<h> g(h, bj<h>), h d, e k, e i, uint64_t j) {
+template < class h>
+void am( bj<h> g(h, bj<h>), h d ) {
+float k = 0.0f;  float i = 1e37f;  uint64_t j = 2;
   using l = uint32_t;
   l m(k);
   l n = al<l>(i);
@@ -149,9 +149,9 @@ void am(e an(e), bj<h> g(h, bj<h>), h d, e k, e i, uint64_t j) {
     l aq = o[ao][1];
     l ar(s);
     for (l as = ap; as <= aq; as += ar) {
-      e q = al<e>(as), ab = an(q);
+      float q = al<float>(as), ab = log1p(q);
       cd<h> r = ci(d, q);
-      e aa = cy(g(d, r));
+      float aa = cy(g(d, r));
       auto ac = aj(aa, ab);
       p = p ?: ac;
     }
@@ -159,9 +159,8 @@ void am(e an(e), bj<h> g(h, bj<h>), h d, e k, e i, uint64_t j) {
   if (!(p <= j))
     abort();
 }
-void bc() {
-  z<float> d;
-  am(log1p, ad, d, 0.0f, 1e37f, 3); 
+int main() {
+    y<float> d;
+    //am( ad, d, 0.0f, 1e37f, 2);
+    am( ad, d) ;
 }
-
-int main() { bc(); }
