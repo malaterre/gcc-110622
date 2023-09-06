a87b304bc8d0295610f82168627389131b32ec4b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-06 13:44:44 +0000

    toto

--- new2/check3.sh
+++ new2/check3.sh
@@ -2,7 +2,8 @@
 set -x
 input=math_test.cc
 #g++ -std=gnu++11 -g -m32 -fexcess-precision=standard -O2 -o works1 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
-g++ -std=c++11 -g -m32 -O2 -o works1 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
+CXX=g++-13
+$CXX -std=c++11 -g -m32 -O2 -o works1 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
@@ -20,7 +21,7 @@ if ! test "$?" = "0"; then
   exit 1
 fi
 #g++ -o fails $input -O2 -Wfatal-errors -Wall -Wextra -lhwy
-g++ -std=gnu++11 -g -m32 -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
+$CXX -std=gnu++11 -g -m32 -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
@@ -28,7 +29,7 @@ fi
 if test "$?" = "0"; then
   exit 1
 fi
-g++ -std=gnu++11 -DDOPRINT -g -m32 -O2 -o works2 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
+$CXX -std=gnu++11 -DDOPRINT -g -m32 -O2 -o works2 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
