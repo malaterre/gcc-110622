405a4c1bebddbd2508c61162e016fde04192f8ac
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-06 14:17:02 +0000

    cvise

--- new2/math_test.cc
+++ new2/math_test.cc
@@ -1,7 +1,5 @@
-#include <assert.h>
 #include <math.h>
 #include <stdint.h>
-#include <stdio.h>
 #define ae(a, b) a < b ? 0 : b
 template <size_t ai, typename aj, typename an> void ao(aj c, an p2) {
   __builtin_memcpy(p2, c, ai);
@@ -9,18 +7,19 @@ template <size_t ai, typename aj, typename an> void ao(aj c, an p2) {
 template <typename aj, typename an> void az(aj *c, an p2) {
   ao<sizeof(aj)>(c, p2);
 }
-template <typename ba> struct e { using bb = ba; };
-template <typename bf> struct bh { bf bi[sizeof(bf)]; };
+template <typename> struct e {};
+template <typename bf> struct bh {
+  bf bi[sizeof(bf)];
+};
+int m_fn1(bool c) { return c ? ~int{} : 0; }
 struct g {
-  static int m_fn1(bool c) { return c ? ~int{} : 0; }
   int bits[sizeof(int)];
 };
-template <class bj> bh<typename bj::bb> bk(bj) {
-  bh<typename bj::bb> i{};
+bh<float> bk(e<float>) {
+  bh<float> i;
   return i;
 }
-//template <class bj> using bn = decltype(bk(bj()));
-template <typename T1, typename T2> bh<T1> br(e<T1>, bh<T2> p2) {
+template <typename T1, typename T2> bh<T1> br(e<T1>, T2 p2) {
   bh<T1> bq;
   az(&p2, &bq);
   return bq;
@@ -79,21 +78,19 @@ bh<float> cg(bh<float> c, bh<float> p2) {
 bh<float> operator*(bh<float> c, bh<float> p2) { return cg(c, p2); }
 g operator==(bh<float> c, bh<float> p2) {
   g m;
-  m.bits[0] = g::m_fn1(c.bi[0] == p2.bi[0]);
+  m.bits[0] = m_fn1(c.bi[0] == p2.bi[0]);
   return m;
 }
 bh<float> bl(bh<float> c, bh<float> p2) { return c + p2; }
 bh<float> bm(bh<float> c, bh<float> p2) { return c - p2; }
 bh<float> bg(bh<float> c, bh<float> p2) { return c * p2; }
 g bp(bh<float> c, bh<float> p2) { return c == p2; }
+bh<float> ad_ay = bs(1.0);
 bh<float> ad(e<float>, bh<float> p2) {
-  bh<float> ay = bs(float(1.0)), u = bl(p2, ay), at;
-#ifdef DOPRINT
-  printf("au0: %g %g \n", u.bi[0], ay.bi[0]);
-#endif
-  auto au = bp(u, ay);
+  bh<float> u = bl(p2, ad_ay), at;
+  auto au = bp(u, ad_ay);
   bh<float> ch = cf(u);
-  auto av = bm(ch, ay);
+  auto av = bm(ch, ad_ay);
   auto ax = bg(at, av);
   return cc(au, p2, ax);
 }
@@ -102,18 +99,17 @@ template <class a, class b> a al(b c) {
   ao<sizeof(f)>(&c, &f);
   return f;
 }
-void am(bh<float> c(e<float>, bh<float>)) {
-  int af = 0, ah = 0;
+int main_af;
+int main() {
+  int ah, as = 528484000;
   e<float> p2;
-  uint32_t as = 528484000;
   float q = al<float>(as), ab(q);
-  bh<float> r = bs(q), ci = c(p2, r);
+  bh<float> r = bs(q), ci = ad(p2, r);
   float ak = ci.bi[0];
-  az(&ak, &af);
+  az(&ak, &main_af);
   az(&ab, &ah);
-  int aw = ah - ae(af, ah);
+  int aw = ah - ae(main_af, ah);
   uint64_t p = aw;
   if (!(p <= 2))
     abort();
 }
-int main() { am(ad); }
