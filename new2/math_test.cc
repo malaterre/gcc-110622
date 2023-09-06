c704aa3faafb3b9b151952f283c047092bc9140d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-06 05:53:23 +0000

    printf solve symptom

--- new2/math_test.cc
+++ new2/math_test.cc
@@ -52,12 +52,16 @@ bh<float> bx(bh<float> c, bh<float> p2, bh<float> k) {
   return by(ca, k);
 }
 bh<float> bz(g c) {
-  bh<float> i;
+  bh<float> i{};
+  printf("bz0: %d %g\n", c.bits[0], i.bi[0]);
   az(&c, &i);
+  printf("bz1: %d %g\n", c.bits[0], i.bi[0]);
   return i;
 }
 bh<float> cc(g c, bh<float> p2, bh<float> k) {
+  printf("cc0: %d\n", c.bits[0]);
   bh cb = bz(c);
+  printf("cc1: %g\n", cb.bi[0]);
   return bx(cb, p2, k);
 }
 bh<float> cf(bh<float> c) {
@@ -93,10 +97,13 @@ int af, ah;
 template <class i, class n> n ad(i, n p2) {
   using ag = typename i::bb;
   n ay = bs(ag(1.0)), u = bl(p2, ay), at;
+//  printf("au0: %g %g \n", u.bi[0], ay.bi[0]);
   auto au = bp(u, ay);
+  printf("au1: %d \n", au.bits[0]);
   bh ch = cf(u);
   auto av = bm(ch, ay);
   auto ax = bg(at, av);
+  printf("ad1: %g %g\n", av.bi[0], ax.bi[0]);
   return cc(au, p2, ax);
 }
 template <class a, class b> a al(b c) {
@@ -109,6 +116,7 @@ void am(bh<float> c(e<float>, bh<float>) ) {
   uint32_t as = 528484000;
   float q = al<float>(as), ab(q);
   bh r = bs(q), ci = c(p2, r);
+  printf("0: %g\n", r.bi[0]);
   float ak = ci.bi[0];
   printf("1: %g %d %d\n", ak, af, ah);
   az(&ak, &af);
