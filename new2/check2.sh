00b112793f377163ddb5675bdc4d88cd5865475e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-06 13:47:49 +0000

    toto

--- new2/check2.sh
+++ new2/check2.sh
@@ -2,7 +2,7 @@
 set -x
 input=math_test.cc
 #g++ -std=c++14 -g -m32 -fexcess-precision=standard -O2 '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o works $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
-g++ -std=c++14 -g -m32 -fexcess-precision=standard -O2  -o works $input -Wfatal-errors -Wall -Wextra -Werror 
+g++ -std=c++11 -g -m32 -fexcess-precision=standard -O2  -o works $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
@@ -12,7 +12,7 @@ if ! test "$?" = "0"; then
 fi
 # do not use O2 just in case
 #clang++-16 -std=c++14 -fsanitize=memory '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o works $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
-clang++-16 -std=c++14 -fsanitize=memory -o works $input -Wfatal-errors -Wall -Wextra -Werror 
+clang++-16 -std=c++11 -fsanitize=memory -o works $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
@@ -22,7 +22,7 @@ if ! test "$?" = "0"; then
 fi
 #g++ -o fails $input -O2 -Wfatal-errors -Wall -Wextra -lhwy
 #g++ -std=c++14 -fexcess-precision=fast -g -m32 -O2 '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o fails $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
-g++ -std=c++14 -fexcess-precision=fast -g -m32 -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror 
+g++ -std=c++11 -fexcess-precision=fast -g -m32 -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
 if ! test "$?" = "0"; then
   exit 1
 fi
