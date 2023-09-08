8583e61e822240e6ec3e9f5fca780addb811fe8a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-08 19:08:22 +0000

    cvise6

--- new3/math_test.cc
+++ new3/math_test.cc
@@ -1,6 +1,196 @@
-#include "hwy/ops/emu128-inl.h"
-#include "hwy/ops/generic_ops-inl.h"
-#include "gtest/gtest_pred_impl.h"
+#include "hwy/ops/shared-inl.h"
+namespace hwy {
+namespace HWY_NAMESPACE {
+template <typename T, size_t N> struct Vec128 {
+  using PrivateT = T;
+  static constexpr size_t kPrivateN = N;
+  T raw[16 / sizeof(T)]{};
+};
+template <typename T, size_t = sizeof(T)> struct Mask128 {
+  static int FromBool(bool b) { return b; }
+  int bits[sizeof(int)];
+};
+template <class V> using DFromV = Simd<typename V::PrivateT, V::kPrivateN, 0>;
+template <class D> Vec128<TFromD<D>, HWY_MAX_LANES_D(D)> Zero(D);
+template <class D> using VFromD = decltype(Zero(D()));
+template <class D, class VFrom> VFromD<D> BitCast(D, VFrom v) {
+  VFromD<D> to;
+  CopySameSize(&v, &to);
+  return to;
+}
+template <class D, typename T2> VFromD<D> Set(D d, T2 t) {
+  VFromD<D> v;
+  for (size_t i = 0; i < MaxLanes(d); ++i)
+    v.raw[i] = t;
+  return v;
+}
+template <typename T, size_t N> void Not(Vec128<T, N> v) {
+  DFromV<decltype(v)> d;
+  RebindToUnsigned<decltype(d)> du;
+  using TU = TFromD<decltype(du)>;
+  VFromD<decltype(d)> vu;
+  for (size_t i = 0; i; ++i)
+    vu.raw[i] = TU();
+}
+template <typename T, size_t N>
+HWY_API Vec128<T, N> And(Vec128<T, N> a, Vec128<T, N> b) {
+  DFromV<decltype(a)> d;
+  RebindToUnsigned<decltype(d)> du;
+  auto au = BitCast(du, a), bu = BitCast(du, b);
+  for (size_t i = 0; i < N; ++i)
+    au.raw[i] &= bu.raw[i];
+  return BitCast(d, au);
+}
+template <typename T, size_t N>
+HWY_API Vec128<T, N> AndNot(Vec128<T, N> a, Vec128<T, N> b) {
+  Not(a);
+  return b;
+}
+template <typename T, size_t N>
+HWY_API Vec128<T, N> Or(Vec128<T, N> a, Vec128<T, N> b) {
+  DFromV<decltype(a)> d;
+  RebindToUnsigned<decltype(d)> du;
+  auto au = BitCast(du, a), bu = BitCast(du, b);
+  for (size_t i = 0; i < N; ++i)
+    au.raw[i] |= bu.raw[i];
+  return BitCast(d, au);
+}
+template <typename T, size_t N>
+HWY_API Vec128<T, N> IfVecThenElse(Vec128<T, N> mask, Vec128<T, N> yes,
+                                   Vec128<T, N> no) {
+  return And(mask, yes), AndNot(mask, no);
+}
+template <typename T, size_t N> Vec128<T, N> VecFromMask(Mask128<T, N> mask) {
+  Vec128<T, N> v;
+  CopySameSize(&mask, &v);
+  return v;
+}
+template <typename T, size_t N>
+HWY_API Vec128<T, N> IfThenElse(Mask128<T, N> mask, Vec128<T, N> yes,
+                                Vec128<T, N> no) {
+  return IfVecThenElse(VecFromMask(mask), yes, no);
+}
+template <int kBits, typename T, size_t N>
+Vec128<T, N> ShiftRight(Vec128<T, N> v) {
+  for (size_t i = 0; i < N; ++i)
+    v.raw[i] = v.raw[i] >> kBits;
+  return v;
+}
+namespace detail {
+template <typename T, size_t N>
+Vec128<T, N> Add(NonFloatTag, Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i) {
+    uint64_t a64(a.raw[i]);
+    uint64_t b64(b.raw[i]);
+    a.raw[i] = a64 + b64;
+  }
+  return a;
+}
+template <typename T, size_t N>
+Vec128<T, N> Sub(NonFloatTag, Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i) {
+    uint64_t a64(a.raw[i]);
+    uint64_t b64(b.raw[i]);
+    a.raw[i] = a64 - b64;
+  }
+  return a;
+}
+template <typename T, size_t N>
+Vec128<T, N> Add(FloatTag, Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i)
+    a.raw[i] += b.raw[i];
+  return a;
+}
+template <typename T, size_t N>
+Vec128<T, N> Sub(FloatTag, Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i)
+    a.raw[i] -= b.raw[i];
+  return a;
+}
+} // namespace detail
+template <typename T, size_t N>
+Vec128<T, N> operator-(Vec128<T, N> a, Vec128<T, N> b) {
+  return detail::Sub(IsFloatTag<T>(), a, b);
+}
+template <typename T, size_t N>
+Vec128<T, N> operator+(Vec128<T, N> a, Vec128<T, N> b) {
+  return detail::Add(IsFloatTag<T>(), a, b);
+}
+namespace detail {
+template <typename T, size_t N>
+Vec128<T, N> Mul(FloatTag, Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i)
+    a.raw[i] *= b.raw[i];
+  return a;
+}
+} // namespace detail
+template <typename T, size_t N>
+Vec128<T, N> operator*(Vec128<T, N> a, Vec128<T, N> b) {
+  return detail::Mul(TypeTag<T>(), a, b);
+}
+template <typename T, size_t N>
+Vec128<T, N> operator/(Vec128<T, N> a, Vec128<T, N> b) {
+  for (size_t i = 0; i < N; ++i)
+    a.raw[i] = a.raw[i] / b.raw[i];
+  return a;
+}
+template <typename T, size_t N>
+Vec128<T, N> MulAdd(Vec128<T, N> mul, Vec128<T, N> x, Vec128<T, N> add) {
+  Vec128<T, N> __trans_tmp_2 = mul * x;
+  return __trans_tmp_2 + add;
+}
+template <typename T, size_t N>
+Vec128<T, N> MulSub(Vec128<T, N> mul, Vec128<T, N> x, Vec128<T, N> sub) {
+  Vec128<T, N> __trans_tmp_3 = mul * x;
+  return __trans_tmp_3 - sub;
+}
+template <typename T, size_t N>
+Mask128<T, N> operator==(Vec128<T, N> a, Vec128<T, N> b) {
+  Mask128<T, N> m;
+  for (size_t i = 0; i; ++i)
+    Mask128<int>::FromBool(a.raw[i] == b.raw[i]);
+  return m;
+}
+template <typename T, size_t N>
+Mask128<T, N> operator<(Vec128<T, N> a, Vec128<T, N> b) {
+  Mask128<T, N> m;
+  for (size_t i = 0; i; ++i) {
+    int __trans_tmp_4 = a.raw[i] < b.raw[i];
+    Mask128<int>::FromBool(__trans_tmp_4);
+  }
+  return m;
+}
+namespace detail {
+template <typename TFrom, typename ToT, size_t N, int kPow2>
+Vec128<ToT, N> ConvertTo(NonFloatTag, Simd<ToT, N, kPow2>,
+                         Vec128<TFrom, N> from) {
+  Vec128<ToT, N> ret;
+  for (size_t i = 0; i < N; ++i)
+    ret.raw[i] = from.raw[i];
+  return ret;
+}
+} // namespace detail
+template <class DTo, typename TFrom, size_t N>
+VFromD<DTo> ConvertTo(DTo d, Vec128<TFrom, N> from) {
+  return detail::ConvertTo(IsFloatTag<TFrom>(), d, from);
+}
+template <typename T, size_t N> T GetLane(Vec128<T, N> v) { return v.raw[0]; }
+template <class D> using Vec = decltype(Zero(D()));
+template <class V> V Add(V a, V b) { return a + b; }
+template <class V> V Sub(V a, V b) { return a - b; }
+template <class V> V Mul(V a, V b) { return a * b; }
+template <class V> V Div(V a, V b) { return a / b; }
+template <class V> auto Eq(V b) {
+  V a;
+  return a == b;
+}
+template <class V> auto Lt(V b) {
+  V a;
+  return a < b;
+}
+} // namespace HWY_NAMESPACE
+} // namespace hwy
+#include "gtest/internal/gtest-internal.h"
 namespace hwy {
 template <typename T, typename TU = MakeUnsigned<T>>
 TU ComputeUlpDelta(T expected, T actual) {
@@ -26,8 +216,7 @@ template <class D, class V> V CallLog1p(D d, V x) { return Log1p(d, x); }
 template <class> struct LogImpl {
   template <class D, class V>
   Vec<Rebind<int64_t, D>> Log2p1NoSubnormal(D, V x) {
-    Rebind<int64_t, D> di64;
-    Rebind<uint64_t, D> du64;
+    Rebind<int64_t, D> di64, du64;
     return Sub(BitCast(di64, ShiftRight<52>(BitCast(du64, x))),
                Set(di64, 1023));
   }
@@ -54,7 +243,7 @@ template <class D, class V, int> V Log(V x) {
   VI kExpScale;
   VI kManMask = Set(di, kIsF32 ? 5 : 4503595332403200);
   VI exp_bits;
-  auto is_denormal = Lt(x, kMinNormal);
+  auto is_denormal = Lt(kMinNormal);
   IfThenElse(is_denormal, kScale, x);
   VI exp_scale = kExpScale;
   Add(exp_scale, impl.Log2p1NoSubnormal(d, exp_bits));
@@ -70,7 +259,7 @@ template <class D, class V> V Log1p(D d, V x) {
   using T = TFromD<D>;
   V kOne = Set(d, T(1.0)), y = Add(x, kOne),
     __trans_tmp_1 = Log<D, V, false>(y);
-  auto is_pole = Eq(y, kOne);
+  auto is_pole = Eq(kOne);
   auto divisor = Sub(y, kOne);
   auto non_pole = Mul(__trans_tmp_1, Div(x, divisor));
   return IfThenElse(is_pole, x, non_pole);
