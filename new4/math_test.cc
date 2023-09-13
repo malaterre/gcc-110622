f8d81ec6ad3a4ffbb016462d8ae521a9c1794bd9
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-13 13:47:03 +0000

    step

--- new4/math_test.cc
+++ new4/math_test.cc
@@ -41,32 +41,6 @@ void TestMath(const char *name, T (fx1)(T),
 #define DEFINE_MATH_TEST(NAME, F32x1, F32xN, F32_MIN, F32_MAX, F32_ERROR,                               F64x1, F64xN, F64_MIN, F64_MAX, F64_ERROR)              struct Test##NAME {                                                              template < class T, class D > void operator()(T, D d) {             TestMath(HWY_STR(NAME), F64x1, F64xN, d, static_cast< T >(F64_MIN),                     static_cast< T >(F64_MAX), F64_ERROR);                              }                                                                            };                                                                             DEFINE_MATH_TEST_FUNC()
 double kNearOneD = BitCast< double >(0x3FEFFFFFFFFFFFFFULL);
 uint64_t Cos64ULP();
-DEFINE_MATH_TEST(, , , , , , ::acos,
-                 CallAcos, 1.0, 1.0, 2)
-DEFINE_MATH_TEST(Acosh, , , , , ,
-                 ::acosh, CallAcosh, 1.0, DBL_MAX,
-                 3) DEFINE_MATH_TEST(Asin, , , , , ,
-                                     ::asin, CallAsin, 1.0, 1.0, 2)
-    DEFINE_MATH_TEST(Asinh, , , , , ,
-                     ::asinh, CallAsinh, DBL_MAX, DBL_MAX,
-                     3) DEFINE_MATH_TEST(Atan, std::atan, CallAtan, -FLT_MAX,
-                                         +FLT_MAX, 3, std::atan, CallAtan,
-                                         -DBL_MAX, +DBL_MAX, 3)
-        DEFINE_MATH_TEST(Atanh, std::atanh, CallAtanh, -kNearOneF, +kNearOneF,
-                         4, std::atanh, CallAtanh, -kNearOneD, +kNearOneD, 3)
-            DEFINE_MATH_TEST(Cos, std::cos, CallCos, -39000.0f, +39000.0f, 3,
-                             std::cos, CallCos, -39000.0, +39000.0, Cos64ULP)
-                DEFINE_MATH_TEST(Exp, std::exp, CallExp, -FLT_MAX, +104.0f, 1,
-                                 std::exp, CallExp, -DBL_MAX, +104.0, 1)
-                    DEFINE_MATH_TEST(Expm1, std::expm1, CallExpm1, -FLT_MAX,
-                                     +104.0f, 4, std::expm1, CallExpm1,
-                                     -DBL_MAX, +104.0, 4)
-                        DEFINE_MATH_TEST(Log, std::log, CallLog, +FLT_MIN,
-                                         +FLT_MAX, 1, std::log, CallLog,
-                                         +DBL_MIN, +DBL_MAX, 1)
-                            DEFINE_MATH_TEST(Log10, std::log10, CallLog10,
-                                             +FLT_MIN, +FLT_MAX, 2, std::log10,
-                                             CallLog10, +DBL_MIN, +DBL_MAX, 2)
                                 DEFINE_MATH_TEST(Log1p, std::log1p, CallLog1p,
                                                  +0.0f, +1e37f, 3, std::log1p,
                                                  CallLog1p, +0.0, +DBL_MAX, 2);
