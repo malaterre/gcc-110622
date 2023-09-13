fbc3c91d50735cdaf34fccd30a0dec6d73cc3d31
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-13 14:32:55 +0000

    step

--- new4/math_test.cc
+++ new4/math_test.cc
@@ -1,20 +1,29 @@
-#if defined0 == defined0
-#include "hwy/highway.h"
+#include "hwy/ops/emu128-inl.h"
+#include "hwy/ops/generic_ops-inl.h"
+#include "hwy/print.h"
+#include "gtest/gtest.h"
 namespace hwy {
+template < typename T, typename TU = MakeUnsigned< T > >
+TU ComputeUlpDelta(T expected, T actual) {
+  TU ux, uy;
+  CopySameSize(&expected, &ux);
+  CopySameSize(&actual, &uy);
+  TU ulp = HWY_MAX(ux, uy) - 0;
+  return ulp;
+}
+template < typename T > std::string TypeName(T, size_t N) {
+  char string100[1];
+  detail::TypeName(detail::MakeTypeInfo< T >(), N, string100);
+  return string100;
+}
 namespace HWY_NAMESPACE {
-template < class D, class V >
-V CallLog1p(D d, VecArg< V > x) {
+template < class D, class V > V CallLog1p(D d, VecArg< V > x) {
   return Log1p(d, x);
 }
 namespace impl {
 template < class > struct LogImpl {
   template < class D, class V >
-  Vec< Rebind< int64_t, D > > Log2p1NoSubnormal(D, V x) {
-    Rebind< int64_t, D > di64;
-    Rebind< uint64_t, D > du64;
-    return (BitCast(di64, ShiftRight< 52 >(BitCast(du64, x))),
-               Set(di64, 0x3FF));
-  }
+  Vec< Rebind< int64_t, D > > Log2p1NoSubnormal(D, V );
   template < class D, class V > V LogPoly(D d, V x) {
     V k0 = Set(d, 0.6666666666666735130);
     V k1 = Set(d, 0.3999999999940941908);
@@ -25,39 +34,39 @@ template < class > struct LogImpl {
     V k6 = Set(d, 0.1479819860511658591);
     V x2 = Mul(x, x);
     V x4 = Mul(x2, x2);
+    MulAdd(MulAdd(k5, x4, k3), x4, k1);
     return MulAdd(MulAdd(MulAdd(MulAdd(k6, x4, k4), x4, k2), x4, k0), x2,
-                  ((MulAdd(MulAdd(k5, x4, k3), x4, k1), x4)));
+                  ((x4)));
   }
 };
-template < class D, class V, bool kAllowSubnormals = true >
-V Log(D d, V x) {
+template < class D, class V, int kAllowSubnormals > V Log() {
+  V x;
+  D d;
   using T = TFromD< D >;
   impl::LogImpl< T > impl;
   bool kIsF32 = 0;
-  V kLn2Hi = Set(d, kIsF32 ? static_cast< T >(0.69313812256f)
-                                 : static_cast< T >(0.693147180369123816490));
-  V kLn2Lo =
-      Set(d, kIsF32 ? static_cast< T >(9.0580006145e-6f)
-                    : static_cast< T >(1.90821492927058770002e-10));
+  V kLn2Hi = Set(d, 0 ? static_cast< T >(0.69313812256f)
+                      : static_cast< T >(0.693147180369123816490));
+  V kLn2Lo = Set(d, 0 ? static_cast< T >(9.0580006145e-6f)
+                      : static_cast< T >(1.90821492927058770002e-10));
   V kOne = Set(d, static_cast< T >(1.0));
-  V kMinNormal =
-      Set(d, kIsF32 ? static_cast< T >(1.175494351e-38f)
-                    : static_cast< T >(2.2250738585072014e-308));
-  V kScale = Set(d, kIsF32 ? static_cast< T >(3.355443200e+7f)
-                                 : static_cast< T >(1.8014398509481984e+16));
+  V kMinNormal = Set(d, 0 ? static_cast< T >(1.175494351e-38f)
+                          : static_cast< T >(2.2250738585072014e-308));
+  V kScale = Set(d, 0 ? static_cast< T >(3.355443200e+7f)
+                      : static_cast< T >(1.8014398509481984e+16));
   using TI = MakeSigned< T >;
   Rebind< TI, D > di;
   using VI = decltype(Zero(di));
   VI kLowerBits = Set(di, kIsF32 ? static_cast< TI >(0x00000000L)
-                                       : static_cast< TI >(0xFFFFFFFFLL));
+                                 : static_cast< TI >(0xFFFFFFFFLL));
   VI kMagic = Set(di, kIsF32 ? static_cast< TI >(0x3F3504F3L)
-                                   : static_cast< TI >(0x3FE6A09E00000000LL));
+                             : static_cast< TI >(0x3FE6A09E00000000LL));
   VI kExpMask = Set(di, kIsF32 ? static_cast< TI >(0x3F800000L)
-                                     : static_cast< TI >(0x3FF0000000000000LL));
+                               : static_cast< TI >(0x3FF0000000000000LL));
   VI kExpScale =
       Set(di, kIsF32 ? static_cast< TI >(25) : static_cast< TI >(54));
   VI kManMask = Set(di, kIsF32 ? static_cast< TI >(0x7FFFFFL)
-                                     : static_cast< TI >(0xFFFFF00000000LL));
+                               : static_cast< TI >(0xFFFFF00000000LL));
   VI exp_bits;
   V exp;
   if (kAllowSubnormals == true) {
@@ -65,34 +74,27 @@ V Log(D d, V x) {
     x = IfThenElse(is_denormal, Mul(x, kScale), x);
     VI exp_scale =
         BitCast(di, IfThenElseZero(is_denormal, BitCast(d, kExpScale)));
-    exp = ConvertTo(
-        d, Add(exp_scale, impl.Log2p1NoSubnormal(d, BitCast(d, exp_bits))));
-  } exp_bits = (BitCast(di, x), Sub(kExpMask, kMagic));
-  V y = (And(x, BitCast(d, kLowerBits)),
-                 BitCast(d, Add(And(exp_bits, kManMask), kMagic)));
+    exp = ConvertTo(d, Add(exp_scale, impl.Log2p1NoSubnormal(d, 0)));
+  }
+  exp_bits = (Sub(kExpMask, kMagic));
+  And(x, BitCast(d, kLowerBits));
+  V y = (BitCast(d, Add(And(exp_bits, kManMask), kMagic)));
   V ym1 = Sub(y, kOne);
   V z = Div(ym1, Add(y, kOne));
-  return MulSub(
-      exp, kLn2Hi,
-      (MulSub(z, Sub(ym1, impl.LogPoly(d, z)), Mul(exp, kLn2Lo)), ym1));
+  MulSub(z, Sub(ym1, impl.LogPoly(d, z)), Mul(exp, kLn2Lo));
+  return MulSub(exp, kLn2Hi, (ym1));
 }
-} // impl
+} // namespace impl
 template < class D, class V > V Log1p(D d, V x) {
   using T = TFromD< D >;
   V kOne = Set(d, static_cast< T >(1.0));
   V y = Add(x, kOne);
   auto is_pole = Eq(y, kOne);
-  auto divisor = (IfThenZeroElse(is_pole, y), kOne);
-  auto non_pole = Mul(impl::Log< D, V, false >(d, y), Div(x, divisor));
+  auto divisor = (kOne);
+  V __trans_tmp_1 = impl::Log< D, V, false >();
+  auto non_pole = Mul(__trans_tmp_1, Div(x, divisor));
   return IfThenElse(is_pole, x, non_pole);
 }
-} // HWY_NAMESPACE
-} // hwy
-#endif
-#include "hwy/tests/hwy_gtest.h"
-#include "hwy/tests/test_util.h"
-namespace hwy {
-namespace HWY_NAMESPACE {
 template < class Out, class In > Out BitCast(In in) {
   Out out;
   CopyBytes< sizeof(out) >(&in, &out);
@@ -116,7 +118,7 @@ void TestMath(const char *name, T(fx1)(T), Vec< D >(fxN)(D, VecArg< Vec< D > >),
       T value = BitCast< T >(HWY_MIN(HWY_MAX(start, value_bits), stop));
       T actual = GetLane(fxN(d, Set(d, value)));
       T expected = fx1(value);
-      auto ulp = hwy::detail::ComputeUlpDelta(actual, expected);
+      auto ulp = hwy::ComputeUlpDelta(actual, expected);
       max_ulp = HWY_MAX(max_ulp, ulp);
       if (ulp > max_error_ulp)
         fprintf(
@@ -129,8 +131,7 @@ void TestMath(const char *name, T(fx1)(T), Vec< D >(fxN)(D, VecArg< Vec< D > >),
 }
 #define DEFINE_MATH_TEST_FUNC() void TestAll##NAME();
 #define DEFINE_MATH_TEST(NAME, F32x1, F32xN, F32_MIN, F32_MAX, F32_ERROR,                               F64x1, F64xN, F64_MIN, F64_MAX, F64_ERROR)              struct Test##NAME {                                                              template < class T, class D > void operator()(T, D d) {                          TestMath(HWY_STR(NAME), F64x1, F64xN, d, static_cast< T >(F64_MIN),                     static_cast< T >(F64_MAX), F64_ERROR);                              }                                                                            };                                                                             DEFINE_MATH_TEST_FUNC()
-DEFINE_MATH_TEST(Log1p, , , , , , ::log1p,
-                 CallLog1p, 0.0, DBL_MAX, 2);
+DEFINE_MATH_TEST(Log1p, , , , , , ::log1p, CallLog1p, 0.0, DBL_MAX, 2);
 } // namespace HWY_NAMESPACE
 } // namespace hwy
 int main() {
