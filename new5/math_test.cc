0b9b370cb7d49b6fbe88b9d7aab0db53cee02b70
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-19 09:02:48 +0000

    step

--- new5/math_test.cc
+++ new5/math_test.cc
@@ -1,5 +1,7 @@
 #include "hwy/contrib/math/math-inl.h"
 #include <iostream>
+#include <inttypes.h>
+
 namespace hwy {
 template <class Out, class In> Out BitCast(In in) {
   Out out;
@@ -7,15 +9,13 @@ template <class Out, class In> Out BitCast(In in) {
   return out;
 }
 template <class D> void TestMath(D d) {
-  uint64_t kSamplesPerRange(4000);
-  uint64_t start = 0;
-  uint64_t stop = 9218868437227405311;
+  uint64_t kSamplesPerRange=4000 , start = 0, stop = 9218868437227405311;
   uint64_t step(stop / kSamplesPerRange);
   for (uint64_t value_bits = start; value_bits <= stop; value_bits += step) {
     double value = BitCast<double>(value_bits),
            actual = GetLane(CallLog1p(d, Set(d, value))),
            expected = log1p(value);
-    fprintf(stderr, "Log1p(%.17g) expected %.17g actual %.17g \n", value,
+    fprintf(stderr, "%" PRIu64 " - Log1p(%.17g) expected %.17g actual %.17g \n", value_bits, value,
             expected, actual);
   }
 }
