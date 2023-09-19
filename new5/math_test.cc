84ab15001904bdf77b77919c2731f6cf59620c65
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-19 08:02:57 +0000

    step

--- new5/math_test.cc
+++ new5/math_test.cc
@@ -2,40 +2,35 @@
 #include <float.h>
 #include <iostream>
 namespace hwy {
-namespace N_EMU128 {
 template <class Out, class In> Out BitCast(In in) {
   Out out;
   CopyBytes<sizeof(out)>(&in, &out);
   return out;
 }
-template <class T, class D>
-void TestMath(T fx1(T), Vec<D> fxN(D, VecArg<Vec<D>>), D d, T min, T max) {
-  using UintT = MakeUnsigned<T>;
-  UintT min_bits(min);
-  UintT max_bits;
-  CopyBytes<sizeof max_bits>(&max, &max_bits);
-  UintT ranges[]{min_bits, max_bits};
-  UintT kSamplesPerRange(4000);
-  UintT start = 0;
-  UintT stop = ranges[1];
-  UintT step(stop / kSamplesPerRange);
-  for (UintT value_bits = start; value_bits <= stop; value_bits += step) {
-    T value = BitCast<T>(value_bits), actual = GetLane(fxN(d, Set(d, value))),
-      expected = fx1(value);
-    fprintf(stderr, "Log1p(%.17g) expected %.17g actual %.17g \n", value,
-            expected, actual);
-  }
-}
 struct TestLog1p {
   template <class T, class D> void operator()(T, D d) {
-    TestMath(log1p, CallLog1p, d, 0.0, DBL_MAX);
+    T min = 0.0, max = DBL_MAX;
+    using UintT = MakeUnsigned<T>;
+    UintT min_bits(min);
+    UintT max_bits;
+    CopyBytes<sizeof max_bits>(&max, &max_bits);
+    UintT ranges[]{min_bits, max_bits};
+    UintT kSamplesPerRange(4000);
+    UintT start = 0;
+    UintT stop = ranges[1];
+    UintT step(stop / kSamplesPerRange);
+    for (UintT value_bits = start; value_bits <= stop; value_bits += step) {
+      T value = BitCast<T>(value_bits),
+        actual = GetLane(CallLog1p(d, Set(d, value))), expected(value);
+      fprintf(stderr, "Log1p(%.17g) expected %.17g actual %.17g \n", value,
+              expected, actual);
+    }
   }
 };
-} // namespace N_EMU128
 } // namespace hwy
 double main_b1;
 int main() {
   hwy::N_EMU128::Simd<double, 1, 0> b2;
-  hwy::N_EMU128::TestLog1p testLog1p;
+  hwy::TestLog1p testLog1p;
   testLog1p(main_b1, b2);
 }
