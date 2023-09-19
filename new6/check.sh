2d973ce4615fd3279682d8abfa58c359d6c20378
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-09-19 09:30:45 +0000

    step

---
+++ new6/check.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -x
+START=0
+STOP=9218868437227405311
+input=math_test.cc
+#g++ -std=c++14 -g -m32 -fexcess-precision=standard -O2 '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o works $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
+#g++ -std=c++11 -g -m32 -fexcess-precision=standard -O2 -o works $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
+g++ -std=c++14 -DHWY_BROKEN_EMU128=0 -DHWY_COMPILE_ONLY_EMU128 -g -m32 -fexcess-precision=standard -O2 -o works $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic -lhwy -lhwy_test
+if ! test "$?" = "0"; then
+  exit 1
+fi
+( ulimit -t 10; ./works $START $STOP )
+if ! test "$?" = "0"; then
+  exit 1
+fi
+./works $START $STOP > /tmp/works.txt 2>&1
+cmp /tmp/works.txt /home/mathieu/Perso/gcc-110622/new5/works.txt
+if ! test "$?" = "0"; then
+  exit 1
+fi
+
+./works $START $STOP 2>&1 | grep --fixed-strings 'Log1p(9.8046570862201476e-17) expected 9.8046570862201476e-17 actual 9.8046570862201476e-17'
+if ! test "$?" = "0"; then
+  exit 1
+fi
+
+# do not use O2 just in case
+#clang++-16 -std=c++14 -fsanitize=memory '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o works $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
+clang++-16 -std=c++14 -DHWY_BROKEN_EMU128=0 -DHWY_COMPILE_ONLY_EMU128 -fsanitize=memory -o works0 $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic -lhwy -lhwy_test 
+if ! test "$?" = "0"; then
+  exit 1
+fi
+( ulimit -t 10; ./works0 $START $STOP )
+if ! test "$?" = "0"; then
+  exit 1
+fi
+#g++ -o fails $input -O2 -Wfatal-errors -Wall -Wextra -lhwy
+#g++ -std=c++14 -fexcess-precision=fast -g -m32 -O2 '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -o fails $input -Wfatal-errors -Wall -Wextra -Werror -lhwy 
+#g++ -std=c++11 -fexcess-precision=fast -g -m32 -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic
+g++ -std=c++14 -DHWY_BROKEN_EMU128=0 -DHWY_COMPILE_ONLY_EMU128 -g -m32 -fexcess-precision=fast -O2 -o fails $input -Wfatal-errors -Wall -Wextra -Werror -Wpedantic -lhwy -lhwy_test
+if ! test "$?" = "0"; then
+  exit 1
+fi
+( ulimit -t 10; ./fails $START $STOP )
+if ! test "$?" = "0"; then
+  exit 1
+fi
+./fails $START $STOP > /tmp/fails.txt 2>&1
+cmp /tmp/fails.txt /home/mathieu/Perso/gcc-110622/new5/fails.txt
+if ! test "$?" = "0"; then
+  exit 1
+fi
+
+./fails $START $STOP 2>&1 | grep --fixed-strings 'Log1p(9.8046570862201476e-17) expected 9.8046570862201476e-17 actual 0 '
+#exit 0
+
+# gcc '-DHWY_DISABLED_TARGETS=(HWY_SSE4|HWY_SSSE3|HWY_SSE2)' -E -P math_test.cc
+
---
+++ new6/math_test.cc
@@ -0,0 +1,31 @@
+#include "hwy/contrib/math/math-inl.h"
+#include <iostream>
+#include <inttypes.h>
+#include <string>
+
+namespace hwy {
+template <class Out, class In> Out BitCast(In in) {
+  Out out;
+  CopyBytes<sizeof(out)>(&in, &out);
+  return out;
+}
+template <class D> void TestMath(D d, uint64_t start, uint64_t stop) {
+  //uint64_t kSamplesPerRange=4000 , start = 0, stop = 9218868437227405311;
+  uint64_t kSamplesPerRange=4000 ;
+  uint64_t step(stop / kSamplesPerRange);
+  for (uint64_t value_bits = start; value_bits <= stop; value_bits += step) {
+    double value = BitCast<double>(value_bits),
+           actual = GetLane(CallLog1p(d, Set(d, value))),
+           expected = log1p(value);
+    fprintf(stderr, "%" PRIu64 " - Log1p(%.17g) expected %.17g actual %.17g %a\n", value_bits, value,
+            expected, actual, actual);
+  }
+}
+} // namespace hwy
+int main(int argc, char *argv[]) {
+	(void)argc;
+  hwy::N_EMU128::Simd<double, 1, 0> b2;
+  uint64_t start =  std::stoull(argv[1]);
+  uint64_t stop = std::stoull (argv[2]);
+  hwy::TestMath(b2, start, stop);
+}
