5205c16a75ad7edb8651378c5071b3c912dee923
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-10-03 10:15:07 +0200

    update

--- pr110643/check5.sh
+++ pr110643/check5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 #g++ -o works inf2.ii -O0 -lhwy -lhwy_test -Wall -Wextra -Wno-narrowing -Wfatal-errors
-g++ -g -o works inf5.cc -O0  -Wall -Wextra -Werror -Wfatal-errors 
+g++ -g -o works inf5.cc -O0 /tmp/arr.cc -Wall -Wextra -Werror -Wfatal-errors 
 if ! test "$?" = "0"; then
   exit 1
 fi
@@ -9,11 +9,19 @@ if ! test "$?" = "0"; then
   exit 1
 fi
 
+clang++-16 -g -o works0 inf5.cc /tmp/arr.cc  -Wall -Wextra -Werror -Wfatal-errors 
+if ! test "$?" = "0"; then
+  exit 1
+fi
+
 #valgrind --leak-check=yes --error-exitcode=1 ./works 
 valgrind --error-exitcode=1 ./works 
+if ! test "$?" = "0"; then
+  exit 1
+fi
 
 #g++ -o fails inf2.ii -O1 -lhwy -lhwy_test -Wall -Wextra -Wno-narrowing -Wfatal-errors
-g++ -g -o fails inf5.cc -O1   -Wall -Wextra -Werror -Wfatal-errors 
+g++ -g -o fails inf5.cc -O1  /tmp/arr.cc -Wall -Wextra -Werror -Wfatal-errors 
 if ! test "$?" = "0"; then
   exit 1
 fi
